af57ef43823892dc2797fe2e0ef105c3185e1249
Benjamin-ESME <benjamin.ducourtioux789@gmail.com>
2026-04-09 19:15:26 +0000

    Chapitre 1 : un commit presque parfait

--- src/lisibilite.py
+++ src/lisibilite.py
@@ -1,1 +1,12 @@
 #Code de benjamin
+def lisibilite(dico):
+    for i in dico:
+        print(i)
+
+def lisibilite_formation(dico,formation):
+    L = []
+    for i in dico:
+        if i["program_id"] == formation:
+            L.append[i]
+    return L
+
