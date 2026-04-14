3193cd4d5430abe75628665cba4ea466c6d27672
Sasha-Terc <sashatercafs@gmail.com>
2026-04-14 11:14:23 +0200

    le début des aventures commençairent

--- src/tri_formation.py
+++ src/tri_formation.py
@@ -1,1 +1,9 @@
 #code de sasha
+def id_formations(dico):
+    liste_forma = []
+    for i in dico :
+        if i[4] not in liste_forma :
+            liste_forma.append(i[4])
+        else : 
+            continue
+    return liste_forma
