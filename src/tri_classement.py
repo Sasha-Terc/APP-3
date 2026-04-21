2deaf021c4ac4458c654599edb41d21259765696
AF060 <antoninfonta378@gmail.com>
2026-04-21 12:13:25 +0000

    un tri magique est apparu

--- src/tri_classement.py
+++ src/tri_classement.py
@@ -1,3 +1,24 @@
 #code de antonin
+
+#### pour les tests seulement
+# mise en forme de la liste pour les tris
+Liste = []
+for d in dico:
+        if d["score"] not in Liste:
+            Liste.append(d["score"])
+print(Liste)        # verif de la liste
+####
+
+
 def tri_classement(liste_dicos):
-    return[[{}],[{}]]
+    if len(liste_dicos) < 2:
+        return liste_dicos
+    e = liste_dicos[0]
+    L1, L2 = [], []
+    for x in liste_dicos[1:]:
+        if x > e:
+            L1.append(x)
+        else:
+            L2.append(x)
+    return tri(L1) + [e] + tri(L2)
+#print(tri(liste_dicos))
