e07331253f2daee569d18ce4b4b837cb6e7d6560
Sasha-Terc <sashatercafs@gmail.com>
2026-04-28 07:13:55 +0000

    je sais pas ce que j'ai fais

--- src/tri_formation.py
+++ src/tri_formation.py
@@ -1,26 +1,9 @@
-#code de sasha
-def recup_formations(file) :  #fonction qui recupere les identifiants de formation dans un fichier et les stocke dans une liste
-    formations = []
-    with open(file, 'r') as f:
-        u = f.readlines()
-        for i in u:
-            formations.append(i.strip())
-    return formations
 
-def slice_formation(formations): #fonction qui divise la liste des formations en deux parties
-    if len(formations) < 2 :
-        return formations[:2]
-    else :
-        mid =   len(formations) // 2
-        l1 = formations[:mid]
-        l2 = formations[mid:]
-        return l1,l2
-
-def tri_formation(l1,l2): #tri fusion en ordre croissant des identifiants de formation 
+def fusion(l1,l2): 
     liste = []
-    i,i=0 
+    i,j=0,0
     while i < len(l1) and j < len(l2):
-        if l1[i] < l2[j]:
+        if l1[i][2] < l2[j][2]:
             liste.append(l1[i])
             i += 1
         else:
@@ -33,3 +16,15 @@ def tri_formation(l1,l2): #tri fusion en ordre croissant des identifiants de for
         liste.append(l2[j])
         j+=1
     return liste
+
+def tri_formation(formations): 
+    if len(formations) < 2 :
+        return formations[:2]
+    else :
+        mid =   len(formations) // 2
+        l1 = formations[:mid]
+        l2 = formations[mid:]
+        return fusion(l1,l2)
+
+
+
