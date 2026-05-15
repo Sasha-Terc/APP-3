034b568ca7c0cd9de77248c81d236f24e52dfedc
Sasha-Terc <sashatercafs@gmail.com>
2026-05-15 00:18:20 +0000

    un commit attention

--- src/tri_classement.py
+++ src/tri_classement.py
@@ -2,7 +2,7 @@ def fusionn(l1,l2):
     liste = []
     i,j=0,0
     while i < len(l1) and j < len(l2):
-        if l1[i]["score"] < l2[j]["score"]:
+        if l1[i]["score"] <= l2[j]["score"]:
             liste.append(l1[i])
             i += 1
         else:
@@ -19,16 +19,10 @@ def fusionn(l1,l2):
 def tri_classement(candidatures): 
     L1 , L2 = [], []
     if len(candidatures) < 2 :
-        return candidatures[:2]  
+        return candidatures[:2]
     else :
-        for i in range(len(candidatures)):
-            while candidatures[i]["program_id"] == candidatures[i+1]["program_id"]:
-                L1.append(candidatures[i])
-            else :
-                L2.append(candidatures[i+1])
-        l2 =tri_classement(tri_classement(L2))
-        l1 = tri_classement(tri_classement(L1))
-    return fusionn(l1,l2)
-          
-    
+        mid =   len(candidatures) // 2
+        l1 = tri_classement(candidatures[:mid])
+        l2 = tri_classement(candidatures[mid:])
+        return fusionn(l1,l2)
 
