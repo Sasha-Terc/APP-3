26d211ba76030e559a288eb5f84155203c79a8a7
Sasha-Terc <sashatercafs@gmail.com>
2026-05-15 14:00:55 +0000

    ça fait 3 fois je reecris la meme chose

--- src/tableau.py
+++ src/tableau.py
@@ -9,46 +9,12 @@ def bench(liste, func):
     t2 = timer()
     return t2 - t1
 
-def generer_liste_aleatoire(n):
-    return [randint(0, 100000) for _ in range(n)]
-
-from random import randint
-from timeit import default_timer as timer
-from tri_classement import tri_classement_fusion, tri_classement_rapide
-from matplotlib import pyplot as plt
-
-def bench(liste, func):
-    t1 = timer()
-    func(liste)
-    t2 = timer()
-    return t2 - t1
-
-def generer_liste_aleatoire(n):
-    return [randint(0, 100000) for _ in range(n)]
-
-from random import randint
-from timeit import default_timer as timer
-from tri_classement import tri_classement_fusion, tri_classement_rapide
-from matplotlib import pyplot as plt
-
-def bench(liste, func):
-    t1 = timer()
-    func(liste)
-    t2 = timer()
-    return t2 - t1
-
-def generer_liste_aleatoire(n):
-    return [randint(0, 100000) for _ in range(n)]
-
-def tableau(sizes, func1, func2):
-    temps_fusion = bench(func1, generer_liste_aleatoire())
-    temps_rapide = bench(func2, generer_liste_aleatoire())  
-
-    
-
-    plt.plot(sizes, temps_fusion, marker="o", label="Tri fusion")
-    plt.plot(sizes, temps_rapide, marker="s", label="Tri rapide")
-    plt.xlabel("Taille de la liste", len(sizes))
+def tableau(liste):
+    t1 = bench(liste, tri_classement_fusion(liste))
+    t2 = bench(liste, tri_classement_rapide(liste))
+    plt.plot([len(liste)], [t1], marker='o', label="Tri fusion")
+    plt.plot([len(liste)], [t2], marker='s', label="Tri rapide")
+    plt.xlabel("Taille de la liste (n)")
     plt.ylabel("Temps d'exécution (secondes)")
     plt.title("Comparaison : Tri fusion vs Tri rapide")
     plt.legend()
